6a216a3976e71725b1d23e0899af04cd3b6f7247
kw-dhshin <mydhshin@gmail.com>
2025-10-05 22:57:58 +0900

    add early exit opimization to bubble sort

--- sort.js
+++ sort.js
@@ -1,10 +1,15 @@
 function sortArray(arr) {
+    let swapped;
+
     for (let i = 0; i < arr.length; i++) {
+        swapped = false;
         for (let j = 0; j < arr.length - i - 1; j++) {
             if (arr[j] > arr[j + 1]) {
                 [arr[j], arr[j + 1]] = [arr[j + 1], arr[j]];
+                swapped = true;
             }
         }
+        if (!swapped) break;
     }
     return arr;
 }
