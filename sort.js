dc128c7eff435fb18b191440fd40e7a1b7386c4d
kw-dhshin <mydhshin@gmail.com>
2025-10-05 22:53:45 +0900

    add ascending/descending order option

--- sort.js
+++ sort.js
@@ -1,7 +1,10 @@
-function sortArray(arr) {
+function sortArray(arr, order = "asc") {
     for (let i = 0; i < arr.length; i++) {
         for (let j = 0; j < arr.length - i - 1; j++) {
-            if (arr[j] > arr[j + 1]) {
+            const condition = order === "asc"
+            ? arr[j] > arr[j + 1]
+            : arr[j] < arr[j + 1];
+            if (condition) {
                 [arr[j], arr[j + 1]] = [arr[j + 1], arr[j]];
             }
         }
@@ -10,6 +13,10 @@ function sortArray(arr) {
 }
 
 
-let arr = [3, 6, 1, 4, 9, 7, 2, 5, 10, 8];
-sortArray(arr);
-console.log(arr);
+let arr1 = [3, 6, 1, 4, 9, 7, 2, 5, 10, 8];
+sortArray(arr1, "asc");
+console.log("Ascending:", arr1);
+
+let arr2 = [3, 6, 1, 4, 9, 7, 2, 5, 10, 8];
+sortArray(arr2, "desc");
+console.log("Descending:", arr2);
